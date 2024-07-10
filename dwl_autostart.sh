92d2aea658ae5073524bd4b5df5ff11abce6bb55
Fred Frey <fred@fpf3.net>
2024-07-10 07:34:18 -0400

    add autostart scripts

---
+++ dwl_autostart.sh
@@ -0,0 +1,4 @@
+nohup swaybg -i '/home/fred/Pictures/Wallpapers/zentile6.png' -m tile &
+nohup dwlb -ipc -font "monospace:size=10" &
+nohup statusbar &
+nohup blueman-applet &
---
+++ dwl_autostart_blocking.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#wal -R
+wlr-randr --output DP-1 --mode 2560x1440@155.0 --output HDMI-A-1 --transform 270 --mode 1920x1080@144.001007
